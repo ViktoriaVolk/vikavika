0cc6e74f047d1ff1e4a15dda93593f8ff87e2b90
ViktorVolk <70575191+ViktorVolk@users.noreply.github.com>
2023-02-22 02:16:15 +0300

    Update github actions1

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,15 +2,9 @@ import requests
 
 api_url = 'http://localhost:8000'
 
-def test_numbers_3_4(self):
-        self.assertEqual(3 * 4, 12)
-def test_numbers_3_3(self):
-        self.assertEqual(3 * 4, 12)
-def test_numbers_3_2(self):
-        self.assertEqual(3 * 4, 12)
 
 class TestDocuments():
     def test_get_empty_docs(self):
         response = requests.get(f'{api_url}/v1/docs')
         assert response.status_code == 200
-        assert len(response.json()) == 0
+        assert len(response.json()) == 2
